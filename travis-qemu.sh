2515a157a546ebfe4d84897f99e96d265afd4a0b
Jeff Waugh <jdub@bethesignal.org>
2016-11-25 09:33:33 +1100

    Dogfood ourselves

---
+++ travis-qemu.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+VERSION=${QEMU_VERSION:=2.7.0}
+ARCHES=${QEMU_ARCHES:=arm aarch64 i386 x86_64}
+
+TARGETS=$(echo $ARCHES | sed 's#$# #;s#\W#-softmmu #g')
+
+echo "VERSION: $VERSION"
+echo "TARGETS: $TARGETS"
+
+test -f "qemu-$VERSION.tar.bz2" || wget "http://wiki.qemu-project.org/download/qemu-$VERSION.tar.bz2"
+tar -xf "qemu-$VERSION.tar.bz2"
+cd "qemu-$VERSION"
+
+./configure \
+  --prefix="$HOME/qemu" \
+  --target-list="$TARGETS" \
+  --disable-docs
+
+make -j4
+make install
